b946ec40efd69126824dd71222c7769194f8d7f2
Aki Salmi <aki.salmi@iki.fi>
2017-10-23 15:35:39 +0300

    making it pass

--- coin-changer.js
+++ coin-changer.js
@@ -2,7 +2,19 @@
 class CoinChanger {
 
   change(amount) {
-    return []
+    return this.recur_f(amount)
+  }
+
+  recur_f(amount, carry=[], coins=[200,100,50,20,10]){
+    const coin = coins[0]
+    if(coins.length === 0){
+      return carry
+    }
+    if( amount >= coin){
+      return this.recur_f(amount-coin, [...carry, coin], coins)
+    }
+    const [first, ...rest] = coins
+    return this.recur_f(amount, carry, rest)
   }
 }
 
--- test/turkufrontend.spec.js
+++ test/turkufrontend.spec.js
@@ -1,7 +1,10 @@
 import jsc from 'jsverify';
 import {expect} from 'chai';
+import R from 'ramda';
 
 import CoinChanger from '../coin-changer'
+import Purse from '../purse'
+import {sents, coins, coin} from './generators'
 
 describe('test env', () => {
   it('should do booleans', () => {
@@ -17,4 +20,83 @@ describe('CoinChanger', () => {
       [200, 200, 200, 50, 20]
     )
   });
+
+  it('returned sum of coins should match', () => {
+    jsc.assertForall(sents, (sents) => {
+      const amount = sents.coins
+      const change = coinChanger.change(amount)
+
+      const c_sum = change.reduce((sum, coin) => sum + coin, 0)
+      return c_sum === amount
+
+    })
+  });
+
+  it('should have 10 cents at most once', () => {
+    jsc.assertForall(sents, (sents) => {
+      const amount = sents.coins
+      const change = coinChanger.change(amount);
+
+      return change.filter(c=>c===10).length <= 1
+    });
+  });
+
+  it('should have all coins, except 2EUR, at max twice', ()=> {
+    jsc.assertForall(sents, (sents) => {
+      const amount = sents.coins
+      const change = coinChanger.change(amount);
+      const purse = new Purse().collect(change);
+
+//      const calc_coin = (c, a, o) => c*a;
+//      const f = R.mapObjIndexed(calc_coin, purse)
+
+      let c = Object.keys(purse).filter(c=>c!=='200').map(
+        c=>purse[c]).filter(x=>x>2);
+
+      return c.length === 0;
+    })
+  })
+
+  it('can have more than 2 coins, if more than 6 EUR', () => {
+    jsc.assertForall(sents, (sents) => {
+      const amount = sents.coins
+      const change = coinChanger.change(amount);
+      const purse = new Purse().collect(change);
+
+//      const calc_coin = (c, a, o) => c*a;
+//      const f = R.mapObjIndexed(calc_coin, purse)
+
+      let c = Object.keys(purse).filter(c=>c===200).map(
+        c=>purse[c]).filter(x=>x>2);
+
+      return c.length < 1;
+    })
+  })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+describe('Purse', () => {
+
+  const purse = new Purse();
+  it('should calculate correctly', ()=>{
+    jsc.assertForall(coins, (arrOfCents) => {
+      const collected = purse.collect(arrOfCents)
+      const amount = arrOfCents.reduce((s,c)=>s+c, 0)
+
+      const res = Object.keys(collected).reduce(
+        (sum, c) => sum + c * collected[c], 0);
+      return res === amount
+    });
+  })
+})
+
